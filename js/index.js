0c46e7b5f33e7881319db75187a3d77c8ca82c56
shuaib shaban <shuaib>
2023-01-04 12:20:49 +0300

    resubmission

--- js/index.js
+++ js/index.js
@@ -1,48 +1,46 @@
-
-const form =document.getElementById("github-form")
-const userList = document.getElementById("user-list")
-const reposlist= document.getElementById("repos-list")
+const form = document.getElementById("github-form");
+const userList = document.getElementById("user-list");
+const reposlist = document.getElementById("repos-list");
 
 // display users
-const getUsers = function(e){
-    e.preventDefault()
-    const user = document.getElementById("search").value
-    fetch(`https://api.github.com/search/users?q=${user}`,{Accept: "application/vnd.github.v3+json"}).then(res => res.json()).then(data =>{
-        console.log(data.items);
-        data.items.map(user=>{
-            const markup = `<li><p>Username: ${user.login}</p> 
+const getUsers = function (e) {
+  e.preventDefault();
+  const user = document.getElementById("search").value;
+  fetch(`https://api.github.com/search/users?q=${user}`, {
+    Accept: "application/vnd.github.v3+json",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.items);
+      data.items.map((user) => {
+        const markup = `<li><p>Username: ${user.login}</p> 
             <img src="${user.avatar_url}" alt="${user.login}">
             <p><a href="${user.html_url}">Go to Profile</a></p>
-            </li>`
-            userList.insertAdjacentHTML("afterbegin", markup)
+            </li>`;
+        userList.insertAdjacentHTML("afterbegin", markup);
 
-            // display user repos on click of user container
-            const userLi = userList.querySelector("li")
-            userLi.addEventListener("click", ()=> displayRepos(user.login))
-
-        })
-       
-        
-    }).catch(err =>{
-        alert(err.message)
+        // display user repos on click of user container
+        const userLi = userList.querySelector("li");
+        userLi.addEventListener("click", () => displayRepos(user.login));
+      });
     })
+    .catch((err) => {
+      alert(err.message);
+    });
+};
 
-}
-
-form.addEventListener("submit", getUsers)
-
+form.addEventListener("submit", getUsers);
 
 // display user repos
-const displayRepos = function(user){
-
-    fetch(`https://api.github.com/users/${user}/repos`).then(res => res.json()).then(data => {
-console.log(data);
-        data.map(repo=>{
-            const markup = `<li><a href="${repo.html_url}">${repo.full_name}</a></li>`
-            userList.innerHTML = ""
-            reposlist.insertAdjacentHTML("afterbegin", markup)
-
-        })
-    })
-   
-}
+const displayRepos = function (user) {
+  fetch(`https://api.github.com/users/${user}/repos`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.map((repo) => {
+        const markup = `<li><a href="${repo.html_url}">${repo.full_name}</a></li>`;
+        userList.innerHTML = "";
+        reposlist.insertAdjacentHTML("afterbegin", markup);
+      });
+    });
+};
